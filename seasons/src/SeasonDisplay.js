d840f67adf4df93d7a6273fdd15d7728a3f5c091
Nika Gamkrelidze <nika.gamkrelidze.1@btu.edu.ge>
2022-07-12 16:17:07 +0400

    1

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,16 @@
+import "./seasonsDisplay.css"
 import React from 'react';
+
+const seasonCongig = {
+    summer: {
+        text:"Let's go to Beach",
+        iconName: 'sun'
+    },
+    winter: {
+        text:"Brrr It's Chilly",
+        iconName: 'snowflake'
+    }
+}
 
 const getSeason = (lat, month) =>{
     if (month > 2 && month < 9) {
@@ -11,15 +23,13 @@ const getSeason = (lat, month) =>{
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, Date().getMonth);
 
-    const text = season === 'winter' ? 'brr its chilly':'lets hit to beach';
-    const icon = season === 'winter' ? 'sun' : 'snowflake';
+    const {text, iconName} = seasonCongig[season]
 
     return (
-    <div>
-        <i classname={`${icon} icon`}></i>
-
+    <div className={`season-dipslay ${season}`}>
+        <i className={`icon-left massive ${iconName} icon`}/>
         <h1>{text}</h1>
-        <i className={`${icon} icon`}/>
+        <i className={`icon-rigth massive ${iconName} icon`}/>
 
     </div>
     )
