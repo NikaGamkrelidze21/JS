bc1d5f8b4a76cf0f04377e25e9de62b4ead1328b
NikaGamkrelidze21 <nika.gamkrelidze.21@btu.edu.ge>
2022-07-28 16:02:15 +0400

    1

---
+++ widgets/src/index.js
@@ -0,0 +1,7 @@
+import { app } from "faker/lib/locales/en"
+import React from "react"
+import ReactDOM from "react"
+import App from "./App"
+
+
+ReactDOM.render(<App />, document.querySelector("#root"))
