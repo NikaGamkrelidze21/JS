dc7c3c534abc51b9103cdaa89f7fe79cecc577b1
NikaGamkrelidze21 <nika.gamkrelidze.21@btu.edu.ge>
2022-07-29 18:41:25 +0400

    Merge branch 'mac' of https://github.com/NikaGamkrelidze21/JS into mac

--- widgets/src/index.js
+++ widgets/src/index.js
@@ -1,7 +1,5 @@
-import { app } from "faker/lib/locales/en"
-import React from "react"
-import ReactDOM from "react"
-import App from "./App"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-
-ReactDOM.render(<App />, document.querySelector("#root"))
+ReactDOM.render(<App />, document.querySelector('#root'));
